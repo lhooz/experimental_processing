63b57e5ab1445f070cedb651dbb7d27e13dbacc1
hao <hao.lee0019@gmail.com>
2022-04-12 02:40:37 +0100

    updated multiple cases kinematics

--- expForces.py
+++ expForces.py
@@ -3,8 +3,32 @@
 import os
 import shutil
 import numpy as np
-from expProcessing_functions import read_exp_data, butter_lowpass, butter_lowpass_filter, kinematics_processing, force_processing, force_transform, write_force_array
+from expProcessing_functions import read_exp_data, expDataAveraging, butter_lowpass, butter_lowpass_filter
+from expProcessing_functions import kinematics_processing, force_processing, force_transform, write_force_array
 
+#-------------case file control----------
+phi = [60.0]
+Re = [1000.0]
+AR = [2.0]
+r1hat = [0.4]
+offset = [0.0]
+ptc = [1.5]
+#-----------------------------------------
+kinematics_file = 'kinematics.dat'
+exp_data_list = []
+for phii in phi:
+    for r1h in r1hat:
+        for ofs in offset:
+            for re in Re:
+                for ar in AR:
+                    for p in ptc:
+                        exp_data_name = 'phi' + '{0:.1f}'.format(
+                            phii) + '__ar' + '{0:.1f}'.format(
+                                ar) + '_ofs' + '{0:.1f}'.format(
+                                    ofs) + '_r1h' + '{0:.1f}'.format(
+                                        r1h) + '__Re' + '{0:.1f}'.format(
+                                            re) + '_ptc' + '{0:.3g}'.format(p)
+                        exp_data_list.append(exp_data_name)
 # -------------input plot control----------
 startTime = 0
 endTime = 32
@@ -13,41 +37,52 @@ outTimeSeries = np.arange(startTime, endTime, timeStep)
 # -----------------------------------------
 gearRatio = 2.49
 # -----------------------------------------
-exp_data_list = [
-    'air_Run_0_034111',
-    'water_Run_0_013157',
-]
-# -----------------------------------------
 range_time = 'all'
 range_value = 'all'
 cycle_time = 1.0
 # ---------------------------------------
-Tweight = [21.45, 3.02]
-legends = ['t750_air', 't750_water']
-#----------------------------------------
 show_range_kinematics = [range_time, range_value]
 # -----------------------------------------
 cwd = os.getcwd()
-data_dir = os.path.join(os.path.dirname(cwd), 'experimental_data/test_data_070422')
-image_out_path = os.path.join(os.path.dirname(cwd), 'processed_results/R070422')
-data_out_path = image_out_path + '/data'
+data_dir = os.path.join(os.path.dirname(cwd),
+                        'experimental_data/expFinal_results')
+kinematics_dir = os.path.join(os.path.dirname(cwd),
+                              'experimental_data/kinematic_cases_expFinal')
+out_dir = os.path.join(os.path.dirname(cwd),
+                       'processed_results/expFinal_processed')
 # -----------------------------------------
-if os.path.exists(image_out_path):
-    shutil.rmtree(image_out_path)
-os.mkdir(image_out_path)
-if os.path.exists(data_out_path):
-    shutil.rmtree(data_out_path)
-os.mkdir(data_out_path)
+if os.path.exists(out_dir):
+    shutil.rmtree(out_dir)
+os.mkdir(out_dir)
 #------------------------------------------
 data_file_all = [f.name for f in os.scandir(data_dir) if f.is_file()]
-data_array = []
+air_data_cases = []
+water_data_cases = []
 for datai in exp_data_list:
-    for data_file in data_file_all:
-        if data_file.startswith(datai):
-            exp_data_filei = os.path.join(data_dir, data_file)
-            expDatai = read_exp_data(exp_data_filei)
-            data_array.append(expDatai)
+    case_out_dir = out_dir + '/' + datai
+    image_out_path = case_out_dir + '/images'
+    data_out_path = case_out_dir + '/data'
 
+    os.mkdir(case_out_dir)
+    os.mkdir(image_out_path)
+    os.mkdir(case_out_dir)
+
+    air_data_array = []
+    water_data_array = []
+    for data_file in data_file_all:
+        if data_file.startswith('air_' + datai):
+            exp_data_filei = os.path.join(data_dir, data_file)
+            zeroDatai, expDatai = read_exp_data(exp_data_filei)
+            air_data_array.append([zeroDatai, expDatai])
+        elif data_file.startswith('water_' + datai):
+            exp_data_filei = os.path.join(data_dir, data_file)
+            zeroDatai, expDatai = read_exp_data(exp_data_filei)
+            water_data_array.append([zeroDatai, expDatai])
+
+        air_case_average = expDataAveraging(air_data_array)
+        water_case_average = expDataAveraging(water_data_array)
+        air_data_cases.append(air_case_average)
+        water_data_cases.append(water_case_average)
 # ---------------------------------------
 # Filter requirements.
 order = 6
@@ -57,22 +92,18 @@ cutoff = 20 / 7.5  # desired cutoff frequency of the filter, Hz
 # Get the filter coefficients so we can check its frequency response.
 sos, b, a = butter_lowpass(cutoff, fs, order=order)
 
-kinematics_arr_processed = kinematics_processing(data_array, gearRatio,
-                                                 outTimeSeries, legends,
-                                                 show_range_kinematics,
-                                                 image_out_path, cycle_time,
-                                                 cutoff, b, a, fs, order)
-force_arr_processed = force_processing(Tweight, data_array, outTimeSeries, legends,
-                                       show_range_kinematics, image_out_path,
-                                       cycle_time, cutoff, b, a, fs, order,
-                                       'yes')
+kinematics_arr_cases = kinematics_processing(
+    exp_data_list, air_data_cases, water_data_cases, gearRatio, outTimeSeries,
+    show_range_kinematics, out_dir, cycle_time, cutoff, b, a, fs, order)
+force_arr_processed = force_processing(Tweight, data_array, outTimeSeries,
+                                       legends, show_range_kinematics,
+                                       image_out_path, cycle_time, cutoff, b,
+                                       a, fs, order, 'yes')
 
-averageKinematics = 0.5 * (kinematics_arr_processed[0] +
-                           kinematics_arr_processed[1])
 netForce = force_arr_processed[1] - force_arr_processed[0]
 
 netForce_transformed = force_transform(Tweight, averageKinematics, netForce,
                                        outTimeSeries, show_range_kinematics,
                                        image_out_path)
-                                       
+
 write_force_array(outTimeSeries, netForce_transformed, data_out_path)
--- expProcessing_functions.py
+++ expProcessing_functions.py
@@ -16,15 +16,49 @@ def read_exp_data(expDataFile):
     """read cfd results force coefficients data"""
     expKinematics = []
     expForce = []
+    zeroKinematics = []
+    zeroForce = []
     with open(expDataFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
+        tstart = 0
 
         for row in csv_reader:
             if line_count < 1:
                 line_count += 1
+            elif line_count < 21:
+                if line_count == 1:
+                    tstart = row[2]
+                t_motor = row[2] - tstart
+                # print(row)
+                motor_flapping = row[3]
+                motor_pitching = row[4]
+                zeroKinematics.append([
+                    float(t_motor),
+                    float(motor_flapping),
+                    float(motor_pitching)
+                ])
+
+                t_balance = row[5] - tstart
+                fx = row[6]
+                fy = row[7]
+                fz = row[8]
+                mx = row[9]
+                my = row[10]
+                mz = row[11]
+                zeroForce.append([
+                    float(t_balance),
+                    float(fx),
+                    float(fy),
+                    float(fz),
+                    float(mx),
+                    float(my),
+                    float(mz),
+                ])
+
+                line_count += 1
             else:
-                t_motor = row[2]
+                t_motor = row[2] - tstart
                 # print(row)
                 motor_flapping = row[3]
                 motor_pitching = row[4]
@@ -34,7 +68,7 @@ def read_exp_data(expDataFile):
                     float(motor_pitching)
                 ])
 
-                t_balance = row[5]
+                t_balance = row[5] - tstart
                 fx = row[6]
                 fy = row[7]
                 fz = row[8]
@@ -55,9 +89,52 @@ def read_exp_data(expDataFile):
 
         print(f'Processed {line_count} lines in {expDataFile}')
 
+    zeroData = [zeroKinematics, zeroForce]
     expData = [expKinematics, expForce]
 
-    return expData
+    return zeroData, expData
+
+
+def expDataAveraging(data_array):
+    """function for averaging multiple exp data"""
+    no_of_cases = len(data_array)
+
+    zero_data_length = []
+    exp_data_length = []
+    for datai in data_array:
+        zeroDatai = datai[0][0]
+        expDatai = datai[1][0]
+        zeroLengthi = len(zeroDatai)
+        expLengthi = len(expDatai)
+        zero_data_length.append(zeroLengthi)
+        exp_data_length.append(expLengthi)
+
+    zero_data_length = np.array(zero_data_length)
+    exp_data_length = np.array(exp_data_length)
+
+    zero_min_length = np.min(zero_data_length)
+    exp_min_length = np.min(exp_data_length)
+
+    zeroKinematics_average = np.zeros([zero_min_length, 3])
+    expKinematics_average = np.zeros([exp_min_length, 3])
+    zeroForce_average = np.zeros([zero_min_length, 7])
+    expForce_average = np.zeros([exp_min_length, 7])
+    for datai in data_array:
+        zeroKinematics_average += datai[0][0][0:zero_min_length]
+        expKinematics_average += datai[1][0][0:exp_min_length]
+        zeroForce_average += datai[0][1][0:zero_min_length]
+        expForce_average += datai[1][1][0:zero_min_length]
+
+    zeroKinematics_average = zeroKinematics_average / no_of_cases
+    expKinematics_average = expKinematics_average / no_of_cases
+    zeroForce_average = zeroForce_average / no_of_cases
+    expForce_average = expForce_average / no_of_cases
+
+    zero_out = [zeroKinematics_average, zeroForce_average]
+    exp_out = [expKinematics_average, expForce_average]
+    out_data_array = [zero_out, exp_out]
+
+    return out_data_array
 
 
 def butter_lowpass(cutoff, fs, order=5):
@@ -82,12 +159,13 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
     return ysos
 
 
-def kinematics_processing(data_array,
+def kinematics_processing(exp_data_list,
+                          air_data,
+                          water_data,
                           gearRatio,
                           outTimeSeries,
-                          legends,
                           show_range_kinematics,
-                          image_out_path,
+                          out_dir,
                           cycle_time,
                           cutoff,
                           b,
@@ -130,70 +208,83 @@ def kinematics_processing(data_array,
         # plt.show()
 
         title = 'refquency respose'
-        out_image_file = os.path.join(image_out_path, title + '.svg')
+        out_image_file = os.path.join(out_dir, title + '.svg')
         fig0.savefig(out_image_file)
 
     legendx = 0.5
     legendy = 1.15
 
-    noOfCases = len(legends)
+    noOfCases = len(exp_data_list)
     range_time = show_range_kinematics[0]
     range_value = show_range_kinematics[1]
 
-    kinematics_arr_processed = []
-    fig, axs = plt.subplots(1, 1)
+    legend = ['air', 'water']
+    kinematics_arr_cases = []
     for i in range(noOfCases):
-        kinematics_array = np.array(data_array[i][0])
-        timei = (kinematics_array[:, 0] -
-                 kinematics_array[0, 0]) / (1000 * cycle_time)
-        # print(timei)
-        kinematics_flapping = kinematics_array[:, 1] - kinematics_array[0, 1]
-        kinematics_pitching = kinematics_array[:, 2] - kinematics_array[0, 2]
+        fig, axs = plt.subplots(1, 1)
+        data_array = [air_data[i], water_data[i]]
+        kinematics_arr_processed = []
+        for j in range(2):
+            kinematics_array = np.array(data_array[j][1][0])
+            timei = (kinematics_array[:, 0] -
+                     kinematics_array[0, 0]) / (1000 * cycle_time)
+            # print(timei)
+            kinematics_flapping = kinematics_array[:, 1] - kinematics_array[0,
+                                                                            1]
+            kinematics_pitching = kinematics_array[:, 2] - kinematics_array[0,
+                                                                            2]
 
-        flappingSpline = UnivariateSpline(timei, kinematics_flapping, s=0)
-        pitchingSpline = UnivariateSpline(timei, kinematics_pitching, s=0)
+            flappingSpline = UnivariateSpline(timei, kinematics_flapping, s=0)
+            pitchingSpline = UnivariateSpline(timei, kinematics_pitching, s=0)
 
-        kinematics_arr = []
-        for ti in outTimeSeries:
-            phii = flappingSpline(ti) / 4096 * 360
-            thetai = (pitchingSpline(ti) -
-                      flappingSpline(ti) / gearRatio) / 4096 * 360
-            # thetai = pitchingSpline(ti) / 4096 * 360
-            kinematic_anglei = [phii, thetai]
-            kinematics_arr.append(kinematic_anglei)
+            kinematics_arr = []
+            for ti in outTimeSeries:
+                phii = flappingSpline(ti) / 4096 * 360
+                thetai = (pitchingSpline(ti) -
+                          flappingSpline(ti) / gearRatio) / 4096 * 360
+                # thetai = pitchingSpline(ti) / 4096 * 360
+                kinematic_anglei = [phii, thetai]
+                kinematics_arr.append(kinematic_anglei)
 
-        kinematics_arr = np.array(kinematics_arr)
-        kinematics_arr_processed.append(kinematics_arr)
+            kinematics_arr = np.array(kinematics_arr)
+            kinematics_arr_processed.append(kinematics_arr)
 
-        axs.plot(outTimeSeries,
-                 kinematics_arr[:, 0],
-                 label=legends[i] + '_flapping')
-        axs.plot(outTimeSeries,
-                 kinematics_arr[:, 1],
-                 label=legends[i] + '_pitching')
+            axs.plot(outTimeSeries,
+                     kinematics_arr[:, 0],
+                     label=legends[j] + '_flapping')
+            axs.plot(outTimeSeries,
+                     kinematics_arr[:, 1],
+                     label=legends[j] + '_pitching')
 
-    if range_time != 'all':
-        axs.set_xlim(range_time)
-    if range_value != 'all':
-        axs.set_ylim(range_value)
+        averageKinematics = 0.5 * (kinematics_arr_processed[0] +
+                                   kinematics_arr_processed[1])
 
-    axs.set_xlabel(r't')
-    axs.set_ylabel(r'angle')
-    axs.label_outer()
+        if range_time != 'all':
+            axs.set_xlim(range_time)
+        if range_value != 'all':
+            axs.set_ylim(range_value)
 
-    axs.legend(loc='upper center',
-               bbox_to_anchor=(legendx, legendy),
-               ncol=len(legends),
-               fontsize='small',
-               frameon=False)
+        axs.set_xlabel(r't')
+        axs.set_ylabel(r'angle')
+        axs.label_outer()
 
-    title = 'kinematics'
-    out_image_file = os.path.join(image_out_path, title + '.svg')
-    fig.savefig(out_image_file)
-    # plt.show()
+        axs.legend(loc='upper center',
+                   bbox_to_anchor=(legendx, legendy),
+                   ncol=len(legends),
+                   fontsize='small',
+                   frameon=False)
 
-    kinematics_arr_processed = np.array(kinematics_arr_processed)
-    return kinematics_arr_processed
+        title = 'kinematics'
+        case_out_dir = out_dir + '/' + exp_data_list[i]
+        image_out_path = case_out_dir + '/images'
+        out_image_file = os.path.join(image_out_path, title + '.svg')
+        fig.savefig(out_image_file)
+        # plt.show()
+
+        kinematics_arr_cases.append(averageKinematics)
+
+    kinematics_arr_cases = np.array(kinematics_arr_processed)
+    return kinematics_arr_cases
 
 
 def force_processing(Tweight,
@@ -414,7 +505,7 @@ def force_transform(Tweight, kinematics_arr, force_arr, outTimeSeries,
         mhi = np.cos(thetai) * myi + np.sin(thetai) * mxi - mh_Buoyancy
         mvi = -1 * np.sin(thetai) * myi + np.cos(thetai) * mxi
         msi = mzi
-        
+
         #---remove direction of mv to resemble drag----
         phiDoti = phiDot(outTimeSeries[i])
         mvi = mvi * np.sign(phiDoti)
@@ -454,7 +545,8 @@ def force_transform(Tweight, kinematics_arr, force_arr, outTimeSeries,
     fig2.savefig(out_image_file)
 
     return force_arr_tranformed
-    
+
+
 def write_force_array(outTimeSeries, force_arr_tranformed, data_out_path):
     """
     function to plot cfd force coefficients results
@@ -465,12 +557,12 @@ def write_force_array(outTimeSeries, force_arr_tranformed, data_out_path):
         for fm in fm_i:
             fm_s = '{0:.10g}'.format(fm)
             fm_str.append(fm_s)
-            
+
         datai = '{0:.10g}'.format(ti) + ',' + ','.join(fm_str)
         data.append(datai)
-        
+
     save_file = data_out_path + '/net_aeroForce_transformed.csv'
-    
+
     with open(save_file, 'w') as f:
         f.write("t(s),fx(N),fy(N),fz(N),mh(Nmm),mv(Nmm),ms(Nmm)\n")
         for item in data:
